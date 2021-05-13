fd59906009d10eb80871127f91eb99cedcb173ca
Pierre V <746601+pierre-vo@users.noreply.github.com>
2021-05-13 16:20:01 +0200

    neovim add default config for gitsigns, add trouble without config

--- nvim_lua/.config/nvim/lua/plugins.lua
+++ nvim_lua/.config/nvim/lua/plugins.lua
@@ -79,4 +79,16 @@ return require('packer').startup(function()
   use { 'hoob3rt/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true}}
   --use { 'glepnir/galaxyline.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true}}
   --use { 'famiu/feline.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true}}
+  use {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
+
 end)
