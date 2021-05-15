ccd897a7942ba0b2d9ac0d04a4f0b9644be7a1ec
Pierre V <>
2021-05-15 13:17:51 +0200

    neovim lua lsp +  autocompletion attempt

--- nvim_lua/.config/nvim/lua/init.lua
+++ nvim_lua/.config/nvim/lua/init.lua
@@ -10,6 +10,7 @@ require('plug_lualine')
 --require('plug_galaxyline')
 --require('plug_feline')
 require('plug_gitsigns')
+require('plug_completion')
 require('plug_lspconfig')
 --require'lsp_signature'.on_attach()
 
