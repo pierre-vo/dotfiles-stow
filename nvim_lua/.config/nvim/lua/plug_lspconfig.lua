e30639c7b1c2517ecc8ff467438516b21947f76f
Pierre V <746601+pierre-vo@users.noreply.github.com>
2021-05-16 00:18:27 +0200

    nvim: lsp lua, match path with Script

--- nvim_lua/.config/nvim/lua/plug_lspconfig.lua
+++ nvim_lua/.config/nvim/lua/plug_lspconfig.lua
@@ -15,7 +15,7 @@ end
 
 -- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
 --local sumneko_root_path = vim.fn.stdpath('cache')..'/lspconfig/sumneko_lua/lua-language-server'
-local sumneko_root_path = '/home/pierre/Temp/lua-language-server'
+local sumneko_root_path = os.getenv("HOME")..'/Scripts/App/lua-language-server'
 local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
 
 require'lspconfig'.sumneko_lua.setup {
