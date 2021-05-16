2197f10490fa194dcd72321f658576c58249fc44
Pierre V <746601+pierre-vo@users.noreply.github.com>
2021-05-16 13:31:16 +0200

    nvim: completion -> compe + dashboard + copy stuff from lunarvim

--- nvim_lua/.config/nvim/lua/keybinds.lua
+++ nvim_lua/.config/nvim/lua/keybinds.lua
@@ -18,3 +18,9 @@ key_mapper('', '<right>', '<nop>')
 --key_mapper('v', 'JK', '<ESC>')
 --key_mapper('v', 'jK', '<ESC>')
 
+
+-- better indenting
+vim.api.nvim_set_keymap('v', '<', '<gv', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '>', '>gv', {noremap = true, silent = true})
+
+
