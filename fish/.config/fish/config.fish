76220947b8bde27c3e6d7d46d60d87cbc2e4fa09
Pierre V <746601+pierre-vo@users.noreply.github.com>
2022-08-19 20:01:56 +0200

    fixup! chore(fish): cleanup + x11/nvidia variables

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,9 +6,10 @@ alias ip="ip --color"
 alias gparted="sudo --preserve-env=XDG_RUNTIME_DIR,WAYLAND_DISPLAY gparted"
 alias usbmount="sudo mount -o rw,gid=984,uid=1000 --target /mnt/USB"
 
-#set MOZ_ENABLE_WAYLAND 1
 set LIBVA_DRIVER_NAME nvidia
+
+#set MOZ_ENABLE_WAYLAND 1
 set MOZ_DISABLE_RDD_SANDBOX 1
 
-set MOZ_ENABLE_WAYLAND 1
 set XDG_SCREENSHOTS_DIR ~/Images/Screenshots
+
