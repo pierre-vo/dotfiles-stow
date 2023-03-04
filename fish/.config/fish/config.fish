8756d1d79cc6976642381184003acb9c70699718
Pierre V <746601+pierre-vo@users.noreply.github.com>
2023-03-04 22:47:31 +0100

    feat(fish): pure fish (https://github.com/pure-fish/pure)

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,10 +1,25 @@
-set fish_greeting
+set -U fish_greeting
 
 fish_add_path ~/.local/bin
 
 alias ip="ip --color"
 alias gparted="sudo --preserve-env=XDG_RUNTIME_DIR,WAYLAND_DISPLAY gparted"
+alias ls='exa --header --icons --group --time-style=long-iso --git -l'
 alias usbmount="sudo mount -o rw,gid=984,uid=1000 --target /mnt/USB"
+alias mntNixos="sudo mount /dev/nvme0n1p8 /mnt/Nixos"
 
-set MOZ_ENABLE_WAYLAND 1
-set XDG_SCREENSHOTS_DIR ~/Images/Screenshots
+set -gx MOZ_ENABLE_WAYLAND 1
+set -gx XDG_SCREENSHOTS_DIR ~/Images/Screenshots
+
+set -gx QT_QPA_PLATFORM wayland
+set -gx QT_QPA_PLATFORMTHEME qt5ct
+set -gx QT_PLATFORMTHEME qt5ct
+set -gx QT_PLATFORM_PLUGIN qt5ct
+set -gx QT_AUTO_SCREEN_SCALE_FACTOR 0
+set -gx QT_SCALE_FACTOR 1
+
+#set -x STARSHIP_CONFIG ~/.config/starship/config.toml
+#set -x STARSHIP_CACHE ~/.config/starship/cache
+
+#starship init fish | source
+
--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,14 +1,12 @@
+# a called to `_pure_prompt_new_line` is triggered by an event
 function fish_prompt
-    if test -n "$SSH_TTY"
-        echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
-    end
+    set --local exit_code $status  # save previous exit code
 
-    echo -n (set_color blue)(prompt_pwd)' '
+    echo -e -n (_pure_prompt_beginning)  # init prompt context (clear current line, etc.)
+    _pure_print_prompt_rows # manage default vs. compact prompt
+    _pure_place_iterm2_prompt_mark # place iTerm shell integration mark
+    echo -e -n (_pure_prompt $exit_code)  # print prompt
+    echo -e (_pure_prompt_ending)  # reset colors and end prompt
 
-    set_color -o
-    if test "$USER" = 'root'
-        echo -n (set_color red)'# '
-    end
-    echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
-    set_color normal
+    set _pure_fresh_session false
 end
