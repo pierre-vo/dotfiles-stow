0ac88f109c11d78ecea2176d70619bd7acae7e8c
Pierre V <746601+pierre-vo@users.noreply.github.com>
2023-08-25 02:36:28 +0200

    feat(tmux): new config? needs cleanup

--- tmux/tmux.sh
+++
@@ -1,5 +0,0 @@
-if which tmux >/dev/null 2>&1; the
-    #if not inside a tmux session, and if no session is started, start a new session
-    test -z "$TMUX" && (tmux attach || tmux new-session -n MainW -s MainS)
-fi
-
